f6f56d7e12bb394e410aeab38fac41a3c4d92ef7
Arturo Castro <castro.ah91@gmail.com>
2020-12-12 21:29:24 -0300

    direccion wishlist correcta

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function validacionFormulario(event){
     if (esExito){
         document.querySelector('#exito').className='';
         $form.className='oculto';
-        setTimeout(function(){window.location.href='file:///C:/Users/Castr/GitHub/tarea8/tarea%20-%20carta%20a%20santa/wishlist.html'}, 5000); 
+        setTimeout(function(){window.location.href='file:///C:/Users/Castr/GitHub/carta-a-santa/wishlist.html'}, 5000); 
     }
 
     event.preventDefault();
